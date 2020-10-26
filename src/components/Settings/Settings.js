2f8c8857a2edf7245eac0b5b3ea71750b5c26dad
Bennie Draaijer <63424221+benniedraaijer@users.noreply.github.com>
2020-10-26 16:36:45 +0100

    Create Settings.js

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../css/students.css";
+
+export const Settings = () => {
+  return (
+    <div className="students-component">
+      <h1 className="students-component-title">Under Construction</h1>
+    </div>
+  );
+};
