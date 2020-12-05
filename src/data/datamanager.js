b76a30fce26c567751ff6586a224a383dac21081
Bennie Draaijer <benniedraaijer@icloud.com>
2020-12-05 09:42:18 +0100

    -

--- src/data/datamanager.js
+++ src/data/datamanager.js
@@ -2,20 +2,19 @@ import { data } from "./database.js";
 
 const students = [];
 const assignments = [];
-
 data.forEach(item => students.push(Object.values(item)[0]));
 data.forEach(item => assignments.push(Object.values(item)[1]));
 
 /////Get Unique Students and Unique Assignments
 export const getStudents = () => Array.from(new Set(students));
-export const getAssignments = () => Array.from(new Set(assignments));
+const getUniqueAssignments = () => Array.from(new Set(assignments));
 
-/////Get Unique Students Objects
+/////Return Unique Student Data
 export const getUniqueStudentResult = student =>
   data.filter(item => item.student === student.student);
 
 export const mainChartData = () => {
-  const ChartData = getAssignments().map(assignment => {
+  const ChartData = getUniqueAssignments().map(assignment => {
     const assignments = data.filter(
       item => Object.values(item)[1] === assignment
     );
@@ -32,6 +31,5 @@ export const mainChartData = () => {
         assigmentLenght,
     };
   });
-
   return ChartData;
 };
